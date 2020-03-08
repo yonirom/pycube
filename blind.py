294af150f5490a9c4752ddc9d150d1b50ad35d19
Jonathan Rom <yonirom@gmail.com>
2020-03-08 11:58:03 +0200

    refactor in progress

--- blind.py
+++ blind.py
@@ -1,5 +1,7 @@
 from functools import partial
+from collections import namedtuple
 import random
+import sys
 
 """""
     The names of the facelet positions of the cube
@@ -41,7 +43,6 @@ colored.fore.YELLOW_1 '\x1b[38;5;226m'
 colored.style.RESET '\x1b[0m'
 
 """
-I = list(range(54))   # Initial cube
 
 COLORS = ['\x1b[38;5;15m'] * 9 + ['\x1b[38;5;2m'] * 9 + ['\x1b[38;5;226m'] * 9 + ['\x1b[38;5;214m'] * 9 + ['\x1b[38;5;1m'] * 9 + ['\x1b[38;5;4m'] * 9
 
@@ -55,18 +56,71 @@ TRANSFORMS = {
         'X': [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 53, 52, 51, 50, 49, 48, 47, 46, 45, 29, 32, 35, 28, 31, 34, 27, 30, 33, 42, 39, 36, 43, 40, 37, 44, 41, 38, 8, 7, 6, 5, 4, 3, 2, 1, 0],
         'Y': [6, 3, 0, 7, 4, 1, 8, 5, 2, 36, 37, 38, 39, 40, 41, 42, 43, 44, 20, 23, 26, 19, 22, 25, 18, 21, 24, 9, 10, 11, 12, 13, 14, 15, 16, 17, 45, 46, 47, 48, 49, 50, 51, 52, 53, 27, 28, 29, 30, 31, 32, 33, 34, 35],
         'Z': [33, 30, 27, 34, 31, 28, 35, 32, 29, 15, 12, 9, 16, 13, 10, 17, 14, 11, 42, 39, 36, 43, 40, 37, 44, 41, 38, 18, 21, 24, 19, 22, 25, 20, 23, 26, 6, 3, 0, 7, 4, 1, 8, 5, 2, 51, 48, 45, 52, 49, 46, 53, 50, 47],
-}
-# U_ = [2, 4, 7, 1, 6, 0, 3, 5, 24, 25, 26, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 40, 41, 42, 27, 28, 29, 30, 31, 8, 9, 10, 35, 36, 37, 38, 39, 32, 33, 34, 43, 44, 45, 46, 47]
-# R_ = [0, 1, 45, 3, 43, 5, 6, 40, 8, 9, 2, 11, 4, 13, 14, 7, 16, 17, 10, 19, 12, 21, 22, 15, 24, 25, 26, 27, 28, 29, 30, 31, 34, 36, 39, 33, 38, 32, 35, 37, 23, 41, 42, 20, 44, 18, 46, 47]
-# L_ = [8, 1, 2, 11, 4, 13, 6, 7, 16, 9, 10, 19, 12, 21, 14, 15, 47, 17, 18, 44, 20, 42, 22, 23, 26, 28, 31, 25, 30, 24, 27, 29, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 5, 43, 3, 45, 46, 0]
-# D  = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 37, 38, 39, 18, 20, 23, 17, 22, 16, 19, 21, 24, 25, 26, 27, 28, 13, 14, 15, 32, 33, 34, 35, 36, 45, 46, 47, 40, 41, 42, 43, 44, 29, 30, 31]
-# F_ = [0, 1, 2, 3, 4, 32, 35, 37, 10, 12, 15, 9, 14, 8, 11, 13, 26, 28, 31, 19, 20, 21, 22, 23, 24, 25, 7, 27, 6, 29, 30, 5, 18, 33, 34, 17, 36, 16, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47]
-# B_ = [29, 27, 24, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 39, 36, 34, 21, 25, 26, 22, 28, 23, 30, 31, 32, 33, 0, 35, 1, 37, 38, 2, 42, 44, 47, 41, 46, 40, 43, 45]
+        }
 
+class Cube():
 
-def A(cube, transform):
-    return [cube[i] for i in transform]
+    def __init__(self, transform):
+        if isinstance(transform, Cube):
+            self.transform = transform.transform
+        else:
+            self.transform = transform
 
+    def _apply(self, transform):
+        return Cube([self.transform[i] for i in transform])
+
+    def __mul__(self, other):
+        return self._apply(other.transform)
+
+    def __pow__(self, exp):
+        c = Cube(self.transform)
+        for i in range(exp - 1):
+            c = c * self
+        return c
+
+    def __repr__(self):
+        return str(self.transform)
+
+    def __str__(self):
+        c = self.transform
+        def f(i):
+            return COLORS[i] + "\u2588\u2588"  #f'{i:2d}'#
+
+        return f"""      {f(c[0])}{f(c[1])}{f(c[2])}
+      {f(c[3])}{f(c[4])}{f(c[5])}
+      {f(c[6])}{f(c[7])}{f(c[8])}
+{f(c[27])}{f(c[28])}{f(c[29])}{f(c[9])}{f(c[10])}{f(c[11])}{f(c[36])}{f(c[37])}{f(c[38])}{f(c[45])}{f(c[46])}{f(c[47])}
+{f(c[30])}{f(c[31])}{f(c[32])}{f(c[12])}{f(c[13])}{f(c[14])}{f(c[39])}{f(c[40])}{f(c[41])}{f(c[48])}{f(c[49])}{f(c[50])}
+{f(c[33])}{f(c[34])}{f(c[35])}{f(c[15])}{f(c[16])}{f(c[17])}{f(c[42])}{f(c[43])}{f(c[44])}{f(c[51])}{f(c[52])}{f(c[53])}
+      {f(c[18])}{f(c[19])}{f(c[20])}
+      {f(c[21])}{f(c[22])}{f(c[23])}
+      {f(c[24])}{f(c[25])}{f(c[26])}
+\x1b[0m"""
+
+I = Cube(list(range(54)))
+
+U = Cube(TRANSFORMS['U'])
+R = Cube(TRANSFORMS['R'])
+L = Cube(TRANSFORMS['L'])
+D = Cube(TRANSFORMS['D'])
+F = Cube(TRANSFORMS['F'])
+B = Cube(TRANSFORMS['B'])
+X = Cube(TRANSFORMS['X'])
+Y = Cube(TRANSFORMS['Y'])
+Z = Cube(TRANSFORMS['Z'])
+
+U2 = Cube(U ** 2)
+Ut = Cube(U ** 3)
+# Tperm = partial(A, transform=(Ft(Rt(U(R(Ut(Rt(Ut(R2(F(Rt(Ut(Rt(U(R(I))))))))))))))))
+print((I * R * U * (R ** 3) * (U ** 3) * (R ** 3) * F * (R ** 2) * (U ** 3) * (R ** 3) * (U ** 3) * R * U * ((R ** 3) * F ** 3)))
+print(I)
+print(Y ** 3)
+
+sys.exit(0)
+
+BlindAlg = namedtuple('BlindAlg', 'wind unwind')
+
+I = list(range(54))   # Initial cube
 
 U = partial(A, transform=TRANSFORMS['U'])
 R = partial(A, transform=TRANSFORMS['R'])
@@ -98,7 +152,7 @@ Mt = partial(A, transform=(M(M(M(I)))))
 E  = partial(A, transform=(U(Dt(Yt(I)))))
 Et = partial(A, transform=(E(E(E(I)))))
 S  = partial(A, transform=(Ft(B(Z(I)))))
-St  = partial(A, transform=(S(S(S(I)))))
+St = partial(A, transform=(S(S(S(I)))))
 
 r = partial(A, transform=(R(Mt(I))))
 rt = partial(A, transform=(Rt(M(I))))
@@ -106,11 +160,37 @@ l = partial(A, transform=(L(M(I))))
 lt = partial(A, transform=(Lt(Mt(I))))
 f = partial(A, transform=(F(S(I))))
 ft = partial(A, transform=(Ft(St(I))))
+d = partial(A, transform=(D(E(I))))
+dt = partial(A, transform=(Dt(Et(I))))
 
 Tperm = partial(A, transform=(Ft(Rt(U(R(Ut(Rt(Ut(R2(F(Rt(Ut(Rt(U(R(I))))))))))))))))
 Yperm = partial(A, transform=(R(U(R2(U(Lt(U2(R(Ut(Rt(U2(R(L(Ut(R(Ut(Rt(I))))))))))))))))))
+bYperm = partial(A, transform=(R(F(Rt(Ut(Rt(U(R(Ft(Rt(U(R(Ut(Rt(Ut(R(I)))))))))))))))))
+Yperm = partial(A, transform=(Ft(bYperm(F(I)))))
 
 JAperm = partial(A, transform=(Xt(U2(r(U(rt(U2(R(Ft(R(F(R2(X(I))))))))))))))
+JBperm = partial(A, transform=(Rt(Ut(R2(F(Rt(Ut(Rt(U(R(Ft(Rt(U(R(I)))))))))))))))
+
+
+BLINDS = {
+    'A': BlindAlg(
+            partial(A, transform=(L2(D(lt(lt(I)))))),
+            partial(A, transform=(lt(lt(Dt(L2(I))))))
+        ),
+    'C': BlindAlg(
+            partial(A, transform=(L2(Dt(lt(lt(I)))))),
+            partial(A, transform=(lt(lt(D(L2(I))))))
+        ),
+    'D': BlindAlg(
+            partial(A, transform=(I)),
+            partial(A, transform=(I)),
+        ),
+    'E': BlindAlg(
+            partial(A, transform=(I)),
+            partial(A, transform=(I)),
+        ),
+}
+
 
 
 
@@ -160,8 +240,13 @@ def main():
     cube = I
     scramble = gen_scramble()
     cube = run_scramble(cube, scramble)
-    print_cube(cube)
-    print_cube(Xt(JAperm(X((cube)))))
+    move = ''
+    while move != 'Q':
+        move = sys.stdin.read(1)
+        if move in globals():
+            cube = globals()[move](cube)
+            print_cube(cube)
+    print_cube(BLINDS['C'].unwind(Tperm(BLINDS['C'].wind(BLINDS['A'].unwind(Tperm(BLINDS['A'].wind((cube))))))))
     print("Scramble: " + scramble)
 
 
